d211b450c3d269a60bf01bf28ac421e3f6565ac6
Rohan Tabish <rohan.tabish@intel.com>
2022-03-09 05:57:37 -0800

    Update the mlc script

--- run_mlc.sh
+++ run_mlc.sh
@@ -2,4 +2,4 @@ ip="10.219.66.160"
 #scp -oStrictHostKeyChecking=no root@${ip}:/usr/local/bin/mlc /usr/local/bin/
 
 result_file=$1_$(getconf _NPROCESSORS_ONLN)
-mlc --max_bandwidth > $result_file
+mlc --loaded_latency -R -t50 -T -d0 > $result_file
