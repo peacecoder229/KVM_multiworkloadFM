6e55c1f0717c1ad07bfce9387320e14dca90fcdc
Muktadir Chowdhury <muktadir.chowdhury@intel.com>
2022-03-21 15:05:50 -0700

    Clean up nutanix testcase script.

--- nutanix_testcases.sh
+++ nutanix_testcases.sh
@@ -1,185 +1,113 @@
-#!/bin/bash
-#Author: Rohan Tabish
-#Organization: Intel Corporation
 
-#################################################################
-# Config # 1: HPVM1(13) | HPVM2 (13) |HVSVM (2) | LP-VM1 (10) | LP-VM2 (10)
-# Config # 2: HPVM1(13)
-# Config # 3: HPVM1 (13) | HPVM2(13)
-# More to be added
-#################################################################
-
-HPVM=34
-LPVM=22
+HPVM=4
+LPVM=2
 
 HPWORKLOAD=$1
 LPWORKLOAD=$2
 
-
-cpupower frequency-set -u 2700Mhz
-pqos -R
-
-
-
-#sed -i "0,/${HPWORKLOAD}_STRING=/s//${HPWORKLOAD}_STRING=${HPWORKLOAD}_solo/" run.sh
-#sed -i "0,/${HPWORKLOAD}_STRING=${HPWORKLOAD}_solo/s//$HPWORKLOAD_STRING=${HPWORKLOAD}_stress/" run.sh
-
-sed -i "s/${HPWORKLOAD}_STRING=/${HPWORKLOAD}_STRING=${HPWORKLOAD}_hp/g" run.sh
-
-
-rm -rf /root/.ssh/known_hosts
-
-virsh list --all --name|xargs -i virsh destroy {} --graceful
-virsh list --all --name|xargs -i virsh undefine {}
-
-echo off > /sys/devices/system/cpu/smt/control
-
-echo "Running HPVM1 Solo"
-
-sed -i 's/"5G" :0/"5G" :1/g' vm_cloud-init.py
-
-./run.sh -T vm -S setup -C $HPVM -W $HPWORKLOAD
-
-sleep 30
-
-virsh list --all --name|xargs -i virsh destroy {} --graceful
-virsh list --all --name|xargs -i virsh start {}
-virsh list
-
-sleep 60
-echo "Starting benchmark now"
-
-#virsh list
-
-./run.sh -T vm -S run -W $HPWORKLOAD
-
-virsh list
-
-
-
-virsh list --all --name|xargs -i virsh destroy {} --graceful
-virsh list --all --name|xargs -i virsh undefine {}
-
-sed -i 's/"5G" :1/"5G" :0/g' vm_cloud-init.py
-rm -rf /home/vmimages2/*
-
-sed -i "s/${HPWORKLOAD}_STRING=${HPWORKLOAD}_hp/${HPWORKLOAD}_STRING=${HPWORKLOAD}_${LPWORKLOAD}/g" run.sh
-
-if [ $LPWORKLOAD != $HPWORKLOAD ]
-then
-	sed -i "s/${LPWORKLOAD}_STRING=/${LPWORKLOAD}_STRING=${LPWORKLOAD}_${HPWORKLOAD}/g" run.sh
-fi
-
-echo "================================================="
-echo "CoScheduling HPVM with LPVM"
-echo "================================================="
-
-sudo dhclient -r $ sudo dhclient
-sed -i 's/"5G" :0/"5G" :2/g' vm_cloud-init.py
-
-./run.sh -T vm -S setup -C $HPVM,$LPVM -W $HPWORKLOAD,$LPWORKLOAD
-
-sleep 30
-
-virsh list --all --name|xargs -i virsh destroy {} --graceful
-virsh list --all --name|xargs -i virsh start {}
-
-sleep 60
-echo "Starting benchmark now"
-
-./run.sh -T vm -S run -W $HPWORKLOAD
-
-
-virsh list --all --name|xargs -i virsh destroy {} --graceful
-virsh list --all --name|xargs -i virsh undefine {}
-
-sed -i 's/"5G" :2/"5G" :0/g' vm_cloud-init.py
-rm -rf /home/vmimages2/*
-
-sed -i "s/${HPWORKLOAD}_STRING=${HPWORKLOAD}_${LPWORKLOAD}/${HPWORKLOAD}_STRING=${HPWORKLOAD}_${LPWORKLOAD}_MBA/g" run.sh
-if [ $LPWORKLOAD != $HPWORKLOAD ]
-then
-	sed -i "s/${LPWORKLOAD}_STRING=${LPWORKLOAD}_${HPWORKLOAD}/${LPWORKLOAD}_STRING=${LPWORKLOAD}_${HPWORKLOAD}_MBA/g" run.sh
-fi
-echo "========================================================="
-echo "CoScheduling HPVM with LPVM with static MBA"
-echo "========================================================="
-
-pqos -a 'core:0=22-55'
-pqos -a 'core:3=0-21'
-
-pqos -e 'mba:0=100'
-pqos -e 'mba:3=10'
-
-
-
-sudo dhclient -r $ sudo dhclient
-sed -i 's/"5G" :0/"5G" :2/g' vm_cloud-init.py
-
-./run.sh -T vm -S setup -C $HPVM,$LPVM -W $HPWORKLOAD,$LPWORKLOAD
-
-sleep 30
-
-virsh list --all --name|xargs -i virsh destroy {} --graceful
-virsh list --all --name|xargs -i virsh start {}
-
-sleep 60
-echo "Starting benchmark now"
-
-./run.sh -T vm -S run -W $HPWORKLOAD
-
-
-virsh list --all --name|xargs -i virsh destroy {} --graceful
-virsh list --all --name|xargs -i virsh undefine {}
-
-
-sed -i 's/"5G" :2/"5G" :0/g' vm_cloud-init.py
-rm -rf /home/vmimages2
-sed -i "s/${HPWORKLOAD}_STRING=${HPWORKLOAD}_${LPWORKLOAD}_MBA/${HPWORKLOAD}_STRING=${HPWORKLOAD}_${LPWORKLOAD}_HWDRC/g" run.sh
-if [ $LPWORKLOAD != $HPWORKLOAD ]
-then
-	sed -i "s/${LPWORKLOAD}_STRING=${LPWORKLOAD}_${HPWORKLOAD}_MBA/${LPWORKLOAD}_STRING=${LPWORKLOAD}_${HPWORKLOAD}_HWDRC/g" run.sh
-fi
-
-
-echo "========================================================="
-echo "CoScheduling HPVM with LPVM with HWDRC"
-echo "========================================================="
-
-pqos -R
-
-cd $PWD/hwdrc_postsi/scripts
-./hwdrc_icx_2S_xcc_init_to_default_pqos_CAS.sh
-cd -
-
-sudo dhclient -r $ sudo dhclient
-sed -i 's/"5G" :0/"5G" :2/g' vm_cloud-init.py
-
-./run.sh -T vm -S setup -C $HPVM,$LPVM -W $HPWORKLOAD,$LPWORKLOAD
-
-sleep 30
-
-virsh list --all --name|xargs -i virsh destroy {} --graceful
-virsh list --all --name|xargs -i virsh start {}
-
-sleep 60
-echo "Starting benchmark now"
-
-./run.sh -T vm -S run -W $HPWORKLOAD
-
-
-virsh list --all --name|xargs -i virsh destroy {} --graceful
-virsh list --all --name|xargs -i virsh undefine {}
-
-
-sed -i 's/"5G" :2/"5G" :0/g' vm_cloud-init.py
-rm -rf /home/vmimages2
-sed -i "s/${HPWORKLOAD}_STRING=${HPWORKLOAD}_${LPWORKLOAD}_HWDRC/${HPWORKLOAD}_STRING=/g" run.sh
-if [ $LPWORKLOAD != $HPWORKLOAD ]
-then
-	sed -i "s/${LPWORKLOAD}_STRING=${LPWORKLOAD}_${HPWORKLOAD}_HWDRC/${LPWORKLOAD}_STRING=/g" run.sh
-fi
-
-
-
-
+function setup_env() {
+  cpupower frequency-set -u 2700Mhz
+  pqos -R
+  rm -rf /root/.ssh/known_hosts
+  echo off > /sys/devices/system/cpu/smt/control
+  
+  destroy_vms
+}
+
+function restart_vms() {
+  virsh list --all --name|xargs -i virsh destroy {} --graceful
+  virsh list --all --name|xargs -i virsh start {}
+  virsh list
+  sleep 60
+}
+
+function destroy_vms() {
+  virsh list
+  virsh list --all --name|xargs -i virsh destroy {} --graceful
+  virsh list --all --name|xargs -i virsh undefine {}
+}
+
+
+function hp_solo_run() {
+  sed -i "s/${HPWORKLOAD}_STRING=/${HPWORKLOAD}_STRING=${HPWORKLOAD}_hp/g" run.sh
+  sed -i 's/"5G" :0/"5G" :1/g' vm_cloud-init.py
+
+  ./run.sh -T vm -S setup -C $HPVM -W $HPWORKLOAD
+  restart_vms 
+  
+  # run the experiment
+  echo "Starting benchmark now ...."
+  ./run.sh -T vm -S run
+  
+  #clean up 
+  destroy_vms
+  sed -i "s/${HPWORKLOAD}_STRING=${HPWORKLOAD}_hp/${HPWORKLOAD}_STRING=/g" run.sh
+  sed -i 's/"5G" :1/"5G" :0/g' vm_cloud-init.py
+  rm -rf /home/vmimages2/*
+}
+
+function hp_lp_corun() {
+  hpworkload_string=${HPWORKLOAD}_${LPWORKLOAD}
+  lpworkload_string=${LPWORKLOAD}_${HPWORKLOAD}
+  if [[ $# -eq 1 ]]; then 
+    mode=$1 # MBA or HWDRC
+    hpworkload_string=${hpworkload_string}_$mode
+    lpworkload_string=${lpworkload_string}_$mode
+  fi
+
+  sed -i "s/${HPWORKLOAD}_STRING=/${HPWORKLOAD}_STRING=${hpworkload_string}/g" run.sh
+  if [ $LPWORKLOAD != $HPWORKLOAD ]
+  then
+    sed -i "s/${LPWORKLOAD}_STRING=/${LPWORKLOAD}_STRING=${lpworkload_string}/g" run.sh
+  fi
+  sed -i 's/"5G" :0/"5G" :2/g' vm_cloud-init.py
+  
+  # set up the VMs
+  sudo dhclient -r $ sudo dhclient
+  ./run.sh -T vm -S setup -C $HPVM,$LPVM -W $HPWORKLOAD,$LPWORKLOAD
+  restart_vms
+
+  # Run experiments in the VMs
+  ./run.sh -T vm -S run
+  
+  # Reset and clean up
+  destroy_vms
+  sed -i 's/"5G" :2/"5G" :0/g' vm_cloud-init.py
+  sed -i "s/${HPWORKLOAD}_STRING=${hpworkload_string}/${HPWORKLOAD}_STRING=/g" run.sh
+  if [ $LPWORKLOAD != $HPWORKLOAD ]
+  then
+    sed -i "s/${LPWORKLOAD}_STRING=${lpworkload_string}/${LPWORKLOAD}_STRING=/g" run.sh
+  fi
+  rm -rf /home/vmimages2/*
+}
+
+function hp_lp_corun_mba() {
+  pqos -a 'core:0=22-55'
+  pqos -a 'core:3=0-21'
+
+  pqos -e 'mba:0=100'
+  pqos -e 'mba:3=10'
+  
+  hp_lp_corun "MBA"
+}
+
+function hp_lp_corun_hwdrc() {
+  pqos -R
+
+  cd $PWD/hwdrc_postsi/scripts
+  ./hwdrc_icx_2S_xcc_init_to_default_pqos_CAS.sh
+  cd -
+
+  hp_lp_corun "HWDRC" 
+}
+
+function main() {
+  setup_env
+  
+  hp_solo_run
+  hp_lp_corun
+  hp_lp_corun_mba
+  hp_lp_corun_hwdrc
+}
+
+main $@ 
